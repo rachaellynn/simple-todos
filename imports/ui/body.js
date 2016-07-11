d6d2acc86eff7f6d04157e4d93a64e5ddfcdd631
rachaellynn <rachael@wordaroundtown.com>
2016-07-11 12:49:35 -0400

    dynamically insert tasks from database

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,11 +1,17 @@
 import { Template } from 'meteor/templating';
  
+import { Tasks } from '../api/tasks.js';
+
 import './body.html';
  
 Template.body.helpers({
-  tasks: [
+  tasks() {
+    return Tasks.find({});
+  },
+  /*tasks: [
     { text: 'This is task 1' },
     { text: 'This is task 2' },
     { text: 'This is task 3' }
   ],
+  */
 });
--- server/main.js
+++ server/main.js
@@ -2,4 +2,7 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  import '../imports/api/tasks.js';
 });
+
+
